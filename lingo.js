0b00b9dde9b1b3708216ecbe3754561616273300
Martine Verburgh <martineverburgh@gmail.com>
2018-06-01 12:14:49 +0200

    Tabel omgezet naar div. Automatisch focus naar volgend textveld.

--- lingo.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-    document.getElementById("R1_L1").innerHTML = "T";
-    document.getElementById("R1_L2").innerHTML = "E";
-    document.getElementById("R1_L3").innerHTML = "S";
-    document.getElementById("R1_L4").innerHTML = "T";
-    document.getElementById("R1_L5").innerHTML = "!";
-}
